2ff3672a1f7db16604a20a8d364fa059bbe72481
Makyrios <43245644+Makyrios@users.noreply.github.com>
2022-10-05 17:49:52 +0300

    Lr1

---
+++ Lr1/Lr1/GameAccount.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lr1
+{
+    class Game
+    {
+        private static int gameIDSEED = 1234567890;
+        public int GameID;
+        public int Rating;
+        public GameAccount Player1;
+        public GameAccount Player2;
+
+        public Game(int rating, GameAccount p1, GameAccount p2)
+        {
+            if (rating < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(rating), "Rating must be positive value");
+            }
+            Rating = rating;
+            Player1 = p1;
+            Player2 = p2;
+            GameID = gameIDSEED++;
+        }
+    }
+
+    class GameAccount
+    {
+        public string UserName { get; set; }
+        public int CurrentRating
+        {
+            get
+            {
+                int rating = 1;
+                foreach (var item in allGames)
+                {
+                    rating = ChangeRating(item, rating);
+                }
+
+                if (rating < 1)
+                    rating = 1;
+
+                return rating;
+            }
+        }
+        public int GamesCount
+        {
+            get
+            {
+                int count = 0;
+                foreach(var item in allGames)
+                {
+                    count++;
+                }
+                return count;
+            }
+        }
+        public List<Game> allGames = new List<Game>();
+
+        public GameAccount(string userName)
+        {
+            UserName = userName;
+        }
+
+        private int ChangeRating(Game game, int rating)
+        {
+            if (game.Player1 == this)
+            {
+                rating += game.Rating;
+            }
+            else
+            {
+                rating -= game.Rating;
+            }
+            return rating;
+        }
+
+        public void WinGame(GameAccount opponent, int rating)
+        {
+            Game game = new Game(rating, this, opponent);
+            allGames.Add(game);
+            opponent.allGames.Add(game);
+        }
+
+        public void LostGame(GameAccount opponent, int rating)
+        {
+            Game game = new Game(rating, opponent, this);
+            allGames.Add(game);
+            opponent.allGames.Add(game);
+        }
+        public string GetStats()
+        {
+            var report = new StringBuilder();
+
+            int rating = 0;
+            report.AppendLine($"{"GameID",-11}{"Opponent",15}{"Rating",10}{"Result",10}");
+            foreach (var item in allGames)
+            {
+                rating = ChangeRating(item, rating);
+                var opponent = item.Player1 == this ? item.Player2 : item.Player1;
+                string getResult = item.Player1 == this ? "Won" : "Lose";
+                report.AppendLine($"{item.GameID,11}{opponent.UserName,15}{item.Rating,10}{getResult,10}");
+            }
+            return report.ToString();
+        }
+    }
+
+    
+}
---
+++ Lr1/Lr1/Program.cs
@@ -0,0 +1,38 @@
+﻿using Lr1;
+
+public class Program
+{
+    public static void Main()
+    {
+        GameAccount player1 = new GameAccount("Player1");
+        GameAccount player2 = new GameAccount("Player2");
+
+        Console.WriteLine($"Current rating of {player1.UserName}: " + player1.CurrentRating);
+        Console.WriteLine($"Current rating of {player2.UserName}: " + player2.CurrentRating);
+        Console.WriteLine($"Current games count of {player1.UserName}: " + player1.GamesCount);
+        Console.WriteLine($"Current games count of {player2.UserName}: " + player2.GamesCount);
+
+
+        player1.WinGame(player2, 20);
+        player2.LostGame(player1, 30);
+        player1.LostGame(player2, 10);
+        player2.WinGame(player1, 0);
+
+        Console.WriteLine(player1.GetStats());
+        Console.WriteLine(player2.GetStats());
+
+        try
+        { 
+        player1.WinGame(player2, -10);
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            Console.WriteLine("Rating must be positive value");
+        }
+
+        Console.WriteLine($"Current rating of {player1.UserName}: " + player1.CurrentRating);
+        Console.WriteLine($"Current rating of {player2.UserName}: " + player2.CurrentRating);
+        Console.WriteLine($"Current games count of {player1.UserName}: " + player1.GamesCount);
+        Console.WriteLine($"Current games count of {player2.UserName}: " + player2.GamesCount);
+    }
+}
